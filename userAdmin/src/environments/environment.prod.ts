573d9ac54fbafd4a9adc836f27435ed88779b8b9
jinalTristate <55229133+jinalTristate@users.noreply.github.com>
2021-12-19 18:31:30 +0530

    Add files via upload

---
+++ userAdmin/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_URL: 'http://localhost:3000/',
+  DEFAULT_AUTH_TOKEN: 'E&@%3SbmT3!QDYbtk!3dD4cB',
+  ENCRYPTION_KEY: 'kljfdsf#jhbNSDJNF38',
+  ENV: 'production'
+};
